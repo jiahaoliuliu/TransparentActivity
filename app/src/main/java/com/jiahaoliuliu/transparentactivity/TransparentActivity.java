d838ac9acb7d1ba02160b7d18d071257f2f004c8
Jiahao Liu <jliu@astrata.eu>
2014-07-21 14:02:04 +0200

    Using point to measure the screen size

--- app/src/main/java/com/jiahaoliuliu/transparentactivity/TransparentActivity.java
+++ app/src/main/java/com/jiahaoliuliu/transparentactivity/TransparentActivity.java
@@ -1,6 +1,7 @@
 package com.jiahaoliuliu.transparentactivity;
 
 import android.app.Activity;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Display;
@@ -9,6 +10,8 @@ import android.view.WindowManager;
 public class TransparentActivity extends Activity {
 
     private static final String TAG = "TransparentActivity";
+
+    private Point screenSize;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,17 +22,25 @@ public class TransparentActivity extends Activity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL,
                 WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL);
 
+        /*
+         * Get the screen size based on a point. This method is available form API 13.
+         * For API before 13, the method display.getWidth() and display.getHeight()
+         * could be used (They are deprecated from API 13)
+         */
         Display display = getWindowManager().getDefaultDisplay();
+        screenSize = new Point();
+        display.getSize(screenSize);
+
         // The follow code has been deprecated, but it works
-        Log.v(TAG, "Size from transparent activity: " + display.getWidth() + ", " + display.getHeight());
+        Log.v(TAG, "Size from transparent activity: " + screenSize.x + ", " + screenSize.y);
 
         WindowManager.LayoutParams params = getWindow().getAttributes();
         // Set the starting point
-        params.x = 0 - display.getWidth()/2;
-        params.y = 0 - display.getHeight()/2;
+        params.x = 0 - screenSize.x/2;
+        params.y = 0 - screenSize.y/2;
 
-        params.width = display.getWidth()/2;
-        params.height = display.getHeight();
+        params.width = screenSize.x/2;
+        params.height = screenSize.y;
 
         getWindow().setAttributes(params);
     }
