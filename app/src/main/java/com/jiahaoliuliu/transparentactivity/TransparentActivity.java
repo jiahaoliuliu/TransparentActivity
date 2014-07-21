fc49461c83f03849e9b9eeabbad40c0c51d5329d
Jiahao Liu <jliu@astrata.eu>
2014-07-21 13:23:37 +0200

    Set the screen windows on the left half part of the screen

--- app/src/main/java/com/jiahaoliuliu/transparentactivity/TransparentActivity.java
+++ app/src/main/java/com/jiahaoliuliu/transparentactivity/TransparentActivity.java
@@ -2,16 +2,35 @@ package com.jiahaoliuliu.transparentactivity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Display;
 import android.view.WindowManager;
 
 public class TransparentActivity extends Activity {
 
+    private static final String TAG = "TransparentActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.transparent_activity_layout);
         // It's needed for first activity to receive touch events
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL,
                 WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL);
+
+        Display display = getWindowManager().getDefaultDisplay();
+        // The follow code has been deprecated, but it works
+        Log.v(TAG, "Size from transparent activity: " + display.getWidth() + ", " + display.getHeight());
+
+        WindowManager.LayoutParams params = getWindow().getAttributes();
+        // Set the starting point
+        params.x = 0 - display.getWidth()/2;
+        params.y = 0 - display.getHeight()/2;
+
+        params.width = display.getWidth()/2;
+        params.height = display.getHeight();
+
+        getWindow().setAttributes(params);
     }
 }
